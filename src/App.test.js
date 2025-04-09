f51c9a37353503b6fdd06741884560ebe699b956
Codec01011 <lildanni006@gmail.com>
2025-04-08 22:56:46 -0600

    added changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders Daniel Eno', () => {
+test('renders class name', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Daniel Eno/i);
+  const linkElement = screen.getByText(/Tech Enterprise/i);
   expect(linkElement).toBeInTheDocument();
 });
